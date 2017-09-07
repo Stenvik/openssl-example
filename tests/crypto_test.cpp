ac55cfeddd0bdcb72350ad9af98198dbc1b76815
Vegard Stenvik <vegardks@gmail.com>
2017-09-07 10:32:37 +0200

    Adds tests for md5,  sha256 and sha512

--- tests/crypto_test.cpp
+++ tests/crypto_test.cpp
@@ -1,14 +1,27 @@
 #include "crypto.hpp"
 #include <cassert>
+#include <iostream>
 
 using namespace std;
 
 int main() {
   // Results copied from https://caligatio.github.io/jsSHA/
+  cout << "Testing SHA1 single" << endl;
   assert(Crypto::hex(Crypto::sha1("Test")) == "640ab2bae07bedc4c163f679a746f7ab7fb5d1fa");
+  
+  cout << "Testing SHA1 3 rounds" << endl;
   assert(Crypto::hex(Crypto::sha1(Crypto::sha1("Test"))) == "af31c6cbdecd88726d0a9b3798c71ef41f1624d5");
+  
+  cout << "Testing SHA256 single" << endl;
+  assert(Crypto::hex(Crypto::sha256("Test")) == "532eaabd9574880dbf76b9b8cc00832c20a6ec113d682299550d7a6e0f345e25");
+  
+  cout << "Testing SHA256 single" << endl;
+  assert(Crypto::hex(Crypto::sha512("Test")) == "c6ee9e33cf5c6715a1d148fd73f7318884b41adcb916021e2bc0e800a5c5dd97f5142178f6ae88c8fdd98e1afb0ce4c8d2c54b5f37b30b7da1997bb33b0b8a31");
 
   // Results copied from http://www.freecodeformat.com/pbkdf2.php
+  cout << "Testing pbkdf2 4096 rounds" << endl;
   assert(Crypto::hex(Crypto::pbkdf2("Password", "Salt", 4096, 128 / 8)) == "f66df50f8aaa11e4d9721e1312ff2e66");
+  
+  cout << "Testing pbkdf2 8192 rounds" << endl;
   assert(Crypto::hex(Crypto::pbkdf2("Password", "Salt", 8192, 512 / 8)) == "a941ccbc34d1ee8ebbd1d34824a419c3dc4eac9cbc7c36ae6c7ca8725e2b618a6ad22241e787af937b0960cf85aa8ea3a258f243e05d3cc9b08af5dd93be046c");
 }
